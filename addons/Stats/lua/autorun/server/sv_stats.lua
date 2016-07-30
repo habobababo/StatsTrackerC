4c4a962b0890ddc73e2291efd12ef79bb62ccb03
habobababo <shadowman11221@gmail.com>
2016-07-30 22:46:14 +0200

    Update sv_stats.lua
    fixed that epic rows

--- addons/Stats/lua/autorun/server/sv_stats.lua
+++ addons/Stats/lua/autorun/server/sv_stats.lua
@@ -1,20 +1,19 @@
 
 local function Stats(ply)
-
-		local plyname = ply:Nick()
-		local steamid = ply:SteamID64()
-		local uid = ply:UniqueID()
-		local UG = ply:GetUserGroup()
-		local NW = ply:GetNWString("usergroup")
-		if ply:IsBot() then return end
-		
-		corequery("SELECT * FROM users WHERE steamid = '"..ply:SteamID64().."' ", function(data)
-			if data[1] != nil then
-					if !IsValid(ply) then return end
-					corequery("UPDATE `users` SET `name` = '"..plyname.."', `UG` = '"..UG.."', `NW` = '"..NW.."' WHERE `steamid` = '"..ply:SteamID64().."'  ")
-			else
-				corequery("INSERT INTO users (steamid, uid, UG, NW, name) VALUES ('"..steamid.."', '"..uid.."', '"..UG.."', '"..NW.."', '"..plyname.."') ")
-			end
-		end)
+	if ply:IsBot() then return end
+	local plyname = ply:Nick()
+	local steamid = ply:SteamID64()
+	local uid = ply:UniqueID()
+	local UG = ply:GetUserGroup()
+	local NW = ply:GetNWString("usergroup")
+	
+	corequery("SELECT * FROM users WHERE steamid = '"..ply:SteamID64().."' ", function(data)
+		if data[1] != nil then
+				if !IsValid(ply) then return end
+				corequery("UPDATE `users` SET `name` = '"..plyname.."', `UG` = '"..UG.."', `NW` = '"..NW.."' WHERE `steamid` = '"..ply:SteamID64().."'  ")
+		else
+			corequery("INSERT INTO users (steamid, uid, UG, NW, name) VALUES ('"..steamid.."', '"..uid.."', '"..UG.."', '"..NW.."', '"..plyname.."') ")
+		end
+	end)
 end
 hook.Add("PlayerInitialSpawn", "StatsTracking",  Stats)
